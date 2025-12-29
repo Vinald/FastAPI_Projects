ed92eed74bf216cae406ccdb9a6ce4684bd9381a
[vinald] <okiror1vinald@gmail.com>
2025-12-29 14:49:47 +0300

    alembic

--- Band/main.py
+++ Band/main.py
@@ -1,18 +1,7 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
 from .api.v1.band import band_router
-from .core.db import init_db
 
-
-@asynccontextmanager
-async def lifespan(apps: FastAPI):
-    init_db()
-    yield
-    # Shutdown code here
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.include_router(band_router, prefix="/bands", tags=["bands"])
